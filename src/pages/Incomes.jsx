d0e332b21423fadeea0341c79539e9fc7642cef1
HasanKazemi <avatarhasan78@gmail.com>
2025-05-11 14:04:31 +0330

    display incomes in a table

--- src/pages/Incomes.jsx
+++ src/pages/Incomes.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import "../styles/incomes.css"
 
 const Incomes = () => {
     const incomes = useSelector(state => state.income)
+    const wallets = useSelector(state => state.wallet)
   return (
-    <div>
-        {incomes.map(income=>(
-            <div key={income.id}>
-                <p>{income.incomeTitle}</p>
-                <p>{income.incomeAmount}</p>
-                <p>{income.toWallet}</p>
-                <p>{income.incomeDate}</p>
-            </div>
-        ))}
+    <div className='incomesTableContainer'>
+      <h1>لیست درآمدها</h1>
+      <table className='incomesTable'>
+        <thead>
+          <tr>
+            <th>ردیف</th>
+            <th>عنوان درآمد</th>
+            <th>مبلغ (ریال)</th>
+            <th>به حساب</th>
+            <th>تاریخ واریز</th>
+          </tr>
+        </thead>
+        <tbody>
+          {incomes.map((income,index)=>(
+            <tr>
+              <td>{index + 1}</td>
+              <td>{income.incomeTitle}</td>
+              <td>{income.incomeAmount.toLocaleString()}</td>
+              <td>{income.toWallet}</td>
+              <td>{income.incomeDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
